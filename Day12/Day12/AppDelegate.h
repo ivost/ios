dc87851a60fbc3e4cc5a48bb88ca8124ea407d5f
Ivo Stoyanov <ivostoy@yahoo.com>
2014-10-07 18:21:21 -0700

    move

---
+++ Day12/Day12/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Day12
+//
+//  Created by Ivo Stoyanov on 10/7/14.
+//  Copyright (c) 2014 HOME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Day12/Day12/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day12
+//
+//  Created by Ivo Stoyanov on 10/7/14.
+//  Copyright (c) 2014 HOME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
